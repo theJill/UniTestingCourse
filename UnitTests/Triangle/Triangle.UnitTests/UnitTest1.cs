153f11843d85570c84f3bb91dcf04b2d75022cb3
Julia Antonovich <antonovich.jl@gmail.com>
2019-10-08 13:36:21 +0300

    Triangle tests with Nunit added

---
+++ UnitTests/Triangle/Triangle.UnitTests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleNameSpace;
+
+namespace TriangleNameSpace.UnitTests
+{
+    [TestClass]
+    public class TriangleUnitTests
+    {
+        [TestMethod]
+        public void IsTriangle_TwoSidesSumLessThenThird_false()
+        {
+            Assert.AreEqual(false, Triangle.IsTriangle(4, 2, 7));
+        }
+
+        [TestMethod]
+        public void IsTriangle_GetsIntAndBoolValues_true()
+        {
+            Assert.AreEqual(true, Triangle.IsTriangle(4, 3.3f, 7));
+        }
+
+        [TestMethod]
+        public void IsTriangle_HasZeroSide_false()
+        {
+            Assert.AreEqual(false, Triangle.IsTriangle(0, 3, 4));
+        }
+
+        [TestMethod()]
+        public void IsTriangle_HasNegativeSide_false()
+        {
+            Assert.AreEqual(false, Triangle.IsTriangle(7, 3, -1));
+        }
+
+        [TestMethod()]
+        public void IsTriangle_TwoSidesSumEqualesThird_false()
+        {
+            Assert.AreEqual(false, Triangle.IsTriangle(7, 3, 4));
+        }
+
+        [TestMethod()]
+        public void IsTriangle_HasPositiveValuesAndSumOfEachTwoMoreThanThird_true()
+        {
+            Assert.AreEqual(true, Triangle.IsTriangle(7, 4, 4));
+        }
+
+        [TestMethod()]
+        public void IsTriangle_IsEqualSided_true()
+        {
+            Assert.AreEqual(true, Triangle.IsEqualsided(4, 4, 4));
+        }
+
+        [TestMethod()]
+        public void IsTriangle_IsRightAngled_true()
+        {
+            Assert.AreEqual(true, Triangle.IsRightAngled(3, 4, 5));
+        }
+
+        [TestMethod()]
+        public void IsTriangle_IsRightAngledConsistingZeroValue_false()
+        {
+            Assert.AreEqual(false, Triangle.IsRightAngled(3, 0, 5));
+        }
+
+        [TestMethod()]
+        public void IsTriangle_AllSidesEqualsZero_false()
+        {
+            Assert.AreEqual(false, Triangle.IsTriangle(0, 0, 0));
+        }
+    }
+}
---
+++ UnitTests/Triangle/Triangle/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleNameSpace
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(float firstSide, float secondSide, float thirdSide)
+        {
+
+            return ((firstSide < secondSide + thirdSide) 
+                && (secondSide < firstSide + thirdSide) 
+                && (thirdSide < firstSide + secondSide)) 
+                &&
+                (firstSide > 0 && secondSide > 0 && thirdSide > 0)
+                ;
+        }
+
+        public static bool IsEqualsided(float firstSide, float secondSide, float thirdSide)
+        {
+            return IsTriangle(firstSide, secondSide, thirdSide) 
+                && (firstSide.Equals(secondSide) && secondSide.Equals(thirdSide));
+        }
+
+        public static bool IsRightAngled(float firstSide, float secondSide, float thirdSide)
+        {
+            float[] allSides = { firstSide, secondSide, thirdSide };
+            return IsTriangle(firstSide, secondSide, thirdSide) 
+                && (Math.Pow(allSides.Distinct().ElementAt(2), 2) ==
+                Math.Pow(allSides.Distinct().ElementAt(1), 2) +
+                Math.Pow(allSides.Distinct().ElementAt(0), 2));
+        }
+    }
+}
