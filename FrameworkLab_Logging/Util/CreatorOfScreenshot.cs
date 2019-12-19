386070f8a55ec43b71aa8fe1cc1ed7cb4e191d40
Julia Antonovich <antonovich.jl@gmail.com>
2019-12-19 18:53:37 +0300

    FR1 FR2 Logging labs added

---
+++ FrameworkLab_Logging/Util/CreatorOfScreenshot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace FrameworkLab
+{
+	public class CreatorOfScreenshot
+	{
+		public static void TakeScreenshot()
+		{
+			ITakesScreenshot screenshot = ((ITakesScreenshot)DriverSingleton.GetDriver());
+			DirectoryInfo directory = Directory.CreateDirectory(@"Screenshots\" + DateTime.Now.ToString("dd_MM_yyyy") + @"\");
+			screenshot.GetScreenshot().SaveAsFile(directory.FullName + @"\" + DateTime.Now.ToString("HH_mm_ss") + ".png", ScreenshotImageFormat.Png);
+			Logger.Log.Info("Take screenshot");
+		}
+	}
+}
